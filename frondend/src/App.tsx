4400758c63150a7230453ce6cbcf2d8ecf59c5de
Tomek Los <tloszabno@gmail.com>
2019-08-08 21:10:42 +0200

    Fix clock widget css and interval cleanup

--- frondend/src/App.tsx
+++ frondend/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import { TodoWidget } from './components/todowidget/TodoWidget'
 import { ClockWidget } from './components/clock/ClockWIdget'
 
 const App: React.FC = () => {
   return (
     <>
-      <div>
-        <TodoWidget />
-      </div>
       <div>
         <ClockWidget />
       </div>
--- frondend/src/components/clock/ClockWIdget.tsx
+++ frondend/src/components/clock/ClockWIdget.tsx
@@ -1,35 +1,39 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import './clock.css'
 import moment from 'moment'
 
-export const ClockWidget: React.FC = () => {
-  const [time, setTime] = useState(moment())
-  const updateTime = () => {
-    setTime(moment())
-  }
-  setInterval(updateTime, 200)
+export const ClockWidget:React.FC = () => {
+    const [time, setTime] = useState(moment())
+    const updateTime = () => {
+        setTime(moment())
+    }
+    useEffect(() => {
+        const timer = setInterval(updateTime, 250)
+        const cleanup = () => clearInterval(timer)
+        return cleanup
+    }, [])
 
-  const hour = time.format('k')
-  const minute = time.format('mm')
-  const second = time.format('ss')
-  const year = time.format('YYYY')
-  const month = time.format('MM')
-  const day = time.format('DD')
+    const hour = time.format('k')
+    const minute = time.format('mm')
+    const second = time.format('ss')
+    const year = time.format('YYYY')
+    const month = time.format('MM')
+    const day = time.format('DD')
 
-  return (
-    <div className="clock-container">
-      <div className="clock-col">
-        <p className="clock-hours clock-timer">{hour}</p>
-        <p className="clock-label">{year}</p>
-      </div>
-      <div className="clock-col">
-        <p className="clock-minutes clock-timer">{minute}</p>
-        <p className="clock-label">{month}</p>
-      </div>
-      <div className="clock-col">
-        <p className="clock-seconds clock-timer">{second}</p>
-        <p className="clock-label">{day}</p>
-      </div>
-    </div>
-  )
+    return (
+        <div className="clock-container">
+            <div className="clock-col">
+                <p className="clock-hours clock-timer">{hour}</p>
+                <p className="clock-label">{year}</p>
+            </div>
+            <div className="clock-col">
+                <p className="clock-minutes clock-timer">{minute}</p>
+                <p className="clock-label">{month}</p>
+            </div>
+            <div className="clock-col">
+                <p className="clock-seconds clock-timer">{second}</p>
+                <p className="clock-label">{day}</p>
+            </div>
+        </div>
+    )
 }
